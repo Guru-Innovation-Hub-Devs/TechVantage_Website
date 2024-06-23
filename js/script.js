0ad265ecc93b8132e00ede6a2526427e6be1cb5b
princetprd <princetprdc@gmail.com>
2024-06-23 01:03:09 +0100

    Created navBar

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Mobile Nav Bar Menu Button
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+});
